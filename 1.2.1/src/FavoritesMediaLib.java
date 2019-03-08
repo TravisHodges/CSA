8cb4b4ff6e044eb733dae8fa10f640a4c07c40df
TravisHodges <37377833+TravisHodges@users.noreply.github.com>
2019-03-08 13:48:47 -0600

    Add files via upload
    1.2.1

---
+++ 1.2.1/src/FavoritesMediaLib.java
@@ -0,0 +1,28 @@
+public class FavoritesMediaLib
+{
+    public static void main(String[] args)
+    {
+        String songInfo = MediaFile.readString();
+
+        System.out.println("My Favorite Songs In Order");
+        System.out.println("---------------------------");
+
+        int index = songInfo.indexOf("|");
+        while (index > 0){
+            String titleOfSong = songInfo.substring(0,index);
+            songInfo = songInfo.substring(index + 1);
+            index = songInfo.indexOf("|");
+            String ratingstring = songInfo.substring(0, index);
+
+            int rating = Integer.valueOf(ratingstring);
+
+            if (rating > 7){
+                System.out.println("Title " + titleOfSong + "(" + rating + ")");
+            }
+            songInfo = songInfo.substring(index + 1);
+            index = songInfo.indexOf("|");
+        }
+
+
+    }
+}
